1640128318b7611c753df775c90a856e36b5349f
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-23 00:06:58 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// For ga oid masalalar
+//1-masala
+// let sum = 0;
+// for (let i = 10; i <= 100; i++) {
+//   if (i % 2 == 1) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+//2-masala
+// let num = +prompt("Sonni kiriting");
+// let p = 1;
+// for (let i = 1; i <= num; i++) {
+//   p *= i;
+// }
+// console.log(p);
+
+//4-masala
+// let sum = 0;
+// for (let i = 1; i <= 20; i++) {
+//   sum += i ** 2;
+// }
+// console.log(sum);
+
+//5-masala
+// for (let i = 1; i <= 5; i++) {
+//   let max;
+//   let min;
+//   let num = +prompt("Sonni kiriting");
+//   if (num > i) {
+//     min = i;
+//     max = num;
+//   } else {
+//     min = num;
+//     max = i;
+//   }
+// }
+// console.log(max, min);
+
+//Break/continue ga oid masalalar.
+//1-masala
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+//2-masala
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 3) {
+//     continue;
+//   }
+//   if (i == 7) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+//3-masala
+// let num = +prompt("Sonni  kiriting");
+// for (let i = 1; i <= num; i++) {
+//   if ((num = 7)) {
+//     console.log("Topildi");
+//     break ;
+//   }
+// }
+
+//4-masala
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+//5-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i > 50) {
+//     break;
+//   }
+//   console.log(i);
+// }
